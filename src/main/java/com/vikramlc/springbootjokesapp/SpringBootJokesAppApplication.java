7537b48d54e107d06a499c617dd6ecf6e9fecba0
vikramlc <vikram.chawan121@gmail.com>
2019-01-13 13:53:57 +0530

    Added XML configuration code

--- src/main/java/com/vikramlc/springbootjokesapp/SpringBootJokesAppApplication.java
+++ src/main/java/com/vikramlc/springbootjokesapp/SpringBootJokesAppApplication.java
@@ -2,8 +2,10 @@ package com.vikramlc.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringBootJokesAppApplication {
 
     public static void main(String[] args) {
